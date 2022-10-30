7af720f0e7e35c1c93239be7874e8d5a8f2d0aa9
yebinge <yebing@student.unimelb.edu.au>
2022-10-30 15:51:18 +1100

    change header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import HotelIcon from "@mui/icons-material/Hotel";
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
         <LunchDiningIcon sx={{ mr: 2 }} />
-        <Typography variant="h5">Melbourne Food</Typography>
+        <HotelIcon sx={{ mr: 2 }} />
+        <Typography variant="h5">Local Food & Shelter</Typography>
       </Toolbar>
     </AppBar>
   );
