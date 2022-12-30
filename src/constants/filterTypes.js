7897bf9aea3be76b3887ffa0a3c500dea25b3977
ethangeau <yebing@student.unimelb.edu.au>
2022-12-30 23:19:20 +1100

    edit filter type

--- src/constants/filterTypes.js
+++ src/constants/filterTypes.js
@@ -1,22 +1,18 @@
 export const types = ["Restaurant", "Cafe", "Bar"];
 
-export const ratings = [
-  "All",
-  "Above 3.0",
-  "Above 3.5",
-  "Above 4.0",
-  "Above 4.5",
-];
+export const ratings = ["All", "Above 3.5", "Above 4.0", "Above 4.5"];
 
 export const cuisines = [
   "All",
   "Chinese",
   "French",
+  "Greek",
+  "Indian",
   "Italian",
   "Japanese",
+  "Lebanese",
   "Malaysian",
+  "Mexican",
   "Thai",
   "Vietnamese",
-  "Lebanese",
-  "Indian",
 ];
