343b2df047e898bb700d3906b03d3da0879835a0
ethangeau <yebing@student.unimelb.edu.au>
2023-01-08 22:58:15 +1100

    update SpotCard

--- src/components/SpotCard.jsx
+++ src/components/SpotCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 import {
+  Avatar,
   Box,
-  Button,
   Card,
   CardContent,
-  CardMedia,
-  CardActions,
   Chip,
   Rating,
   Typography,
+  Link,
 } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import PhoneIcon from "@mui/icons-material/Phone";
@@ -20,68 +19,110 @@ export default function SpotCard({ spot, selected, refProp }) {
   }
   if (selected) console.log(selected);
 
+  const handleOnClick = () => {
+    console.log("reviews clicked");
+  };
+
   return (
-    <Card>
-      <CardMedia
-        sx={{ height: 350 }}
-        title={spot.name}
-        image={
-          spot.photo
-            ? spot.photo.images.large.url
-            : "https://www.pexels.com/zh-cn/photo/225228/"
-        }
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          {spot.name}
-        </Typography>
-        <Box display="flex" justifyContent="space-between">
-          <Rating value={Number(spot.rating)} readOnly />
-          <Typography gutterBottom variant="subtitle1">
-            Based on {spot.num_reviews} reviews
-          </Typography>
+    <Card
+      raised
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        mb: "1px",
+      }}
+    >
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", width: "75%" }}>
+          <CardContent sx={{ pb: "10px !important" }}>
+            <Typography gutterBottom variant="h6">
+              <Link
+                href={spot.website}
+                underline="hover"
+                color="inherit"
+                target="_blank"
+              >
+                {spot.name}
+              </Link>
+            </Typography>
+            <Box display="flex">
+              <Typography gutterBottom variant="body2" sx={{ mr: "2px" }}>
+                {parseFloat(spot.rating).toFixed(1)}
+              </Typography>
+              <Rating
+                value={Number(spot.rating)}
+                precision={0.1}
+                readOnly
+                size="small"
+                sx={{ mr: "2px" }}
+              />
+              <Typography
+                gutterBottom
+                variant="body2"
+                onClick={handleOnClick}
+                sx={{
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+              >
+                {spot.num_reviews} reviews
+              </Typography>
+            </Box>
+            <Box display="flex">
+              <Typography variant="body2" gutterBottom>
+                {spot.price_level}
+              </Typography>
+            </Box>
+            {spot.address && (
+              <Typography
+                gutterBottom
+                variant="body2"
+                display="flex"
+                alignItems="center"
+              >
+                <LocationOnIcon fontSize="small" />
+                {spot.address.split(",")[0]}
+              </Typography>
+            )}
+            {spot.phone && (
+              <Typography variant="body2" display="flex" alignItems="center">
+                <PhoneIcon fontSize="small" sx={{ mr: "5px" }} /> {spot.phone}
+              </Typography>
+            )}
+          </CardContent>
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Price Level</Typography>
-          <Typography variant="subtitle1">{spot.price_level}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            width: "25%",
+            mt: 3,
+          }}
+        >
+          <Avatar
+            alt={spot.name}
+            src={
+              spot.photo
+                ? spot.photo.images.large.url
+                : "https://www.pexels.com/zh-cn/photo/225228/"
+            }
+            sx={{ width: 80, height: 80, borderRadius: 2 }}
+          />
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Ranking</Typography>
-          <Typography variant="subtitle1">{spot.ranking}</Typography>
-        </Box>
-        {spot.address && (
-          <Typography
-            gutterBottom
-            variant="body2"
-            display="flex"
-            justifyContent="space-between"
-          >
-            <LocationOnIcon />
-            {spot.address}
-          </Typography>
-        )}
-        {spot.phone && (
-          <Typography
-            variant="body2"
-            display="flex"
-            justifyContent="space-between"
-          >
-            <PhoneIcon /> {spot.phone}
-          </Typography>
-        )}
-        {spot.cuisine?.map(({ key, name }) => (
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexWrap: "wrap",
+          mx: "1rem",
+          mb: "5px",
+        }}
+      >
+        {spot.cuisine?.slice(0, 4).map(({ key, name }) => (
           <Chip key={key} size="small" label={name} />
         ))}
-      </CardContent>
-      <CardActions>
-        <Button
-          size="small"
-          color="primary"
-          onClick={() => window.open(spot.website, "_blank")}
-        >
-          Website
-        </Button>
-      </CardActions>
+      </Box>
     </Card>
   );
 }
