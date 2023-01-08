5e02cf9cc5a9f81186e6c1c15425e819b28e9726
ethangeau <yebing@student.unimelb.edu.au>
2023-01-08 16:58:49 +1100

    add Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const handleKeyPress = () => {};
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "1rem",
+        left: "1rem",
+        zIndex: 1,
+        backgroundColor: "#fff",
+      }}
+    >
+      <TextField
+        onKeyDown={handleKeyPress}
+        name="search"
+        value={search}
+        placeholder="name or cuisine"
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => setSearch(e.target.value)}
+        sx={{
+          width: "15rem",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Search;
