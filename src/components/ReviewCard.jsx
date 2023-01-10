96c4f2c359adb101341f27c24fc856d8ed8a6ba4
ethangeau <yebing@student.unimelb.edu.au>
2023-01-10 11:10:11 +1100

    update ReviewCard

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import {
+  Avatar,
   Card,
   CardHeader,
   CardContent,
   Typography,
-  Avatar,
   Rating,
   Button,
 } from "@mui/material";
 
 const ReviewCard = ({ review }) => {
-  const { name, rating, time, text } = review;
+  const {
+    author_name,
+    profile_photo_url,
+    rating,
+    text,
+    relative_time_description,
+  } = review;
   const [isOverflowVisible, setIsOverflowVisible] = useState(false);
 
   const handleMoreClick = () => {
@@ -22,16 +28,21 @@ const ReviewCard = ({ review }) => {
     : {
         overflow: "hidden",
         display: "-webkit-box",
-        WebkitLineClamp: 4,
+        WebkitLineClamp: 3,
         WebkitBoxOrient: "vertical",
       };
 
   return (
     <Card raised>
       <CardHeader
-        avatar={<Avatar>{name[0].toUpperCase()}</Avatar>}
-        title={name}
-        subheader={time}
+        avatar={
+          <Avatar
+            alt={author_name && author_name[0].toUpperCase()}
+            src={profile_photo_url}
+          />
+        }
+        title={author_name}
+        subheader={relative_time_description}
       />
       <CardContent sx={{ py: 0 }}>
         <Rating value={Number(rating)} size="small" readOnly />
