9e6baf98d95c9894ef5bae37b57c7706d94aa56d
ethangeau <yebing@student.unimelb.edu.au>
2023-01-20 11:26:56 +1100

    add Marker in Google Map

--- src/components/NewMap.jsx
+++ src/components/NewMap.jsx
@@ -3,7 +3,12 @@ import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 import React from "react";
 
-function NewMap() {
+function NewMap({
+  spots,
+  selectedSpotId,
+  setSelectedSpotId,
+  setDisplayDetail,
+}) {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -39,7 +44,28 @@ function NewMap() {
             zoom={15}
             onLoad={onLoad}
             options={options}
-          ></GoogleMap>
+          >
+            {spots?.map((spot) => (
+              <Marker
+                key={spot.place_id}
+                position={{
+                  lat: spot.geometry.location.lat,
+                  lng: spot.geometry.location.lng,
+                }}
+                icon={{
+                  url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=80&photo_reference=${spot.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`,
+                  scaledSize:
+                    spot.place_id === selectedSpotId
+                      ? { width: 80, height: 80 }
+                      : { width: 40, height: 40 },
+                }}
+                onClick={() => {
+                  setSelectedSpotId(spot.place_id);
+                  setDisplayDetail(true);
+                }}
+              />
+            ))}
+          </GoogleMap>
         </>
       ) : (
         <h1>Google Map is loading</h1>
